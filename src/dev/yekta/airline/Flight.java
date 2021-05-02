1a2cf1927fd24bf1ef52c76e50a3bd01ca8d64eb
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-05-02 20:47:41 +0430

    Update Flight.java

--- src/dev/yekta/airline/Flight.java
+++ src/dev/yekta/airline/Flight.java
@@ -20,6 +20,66 @@
 
 package dev.yekta.airline;
 
-public class Flight {
+import java.util.Objects;
 
+import static dev.yekta.airline.ConsoleFormat.*;
+
+public class Flight {
+    public static final String[] FLIGHT_WEEKDAYS = new String[]{"Saturday", "Sunday", "Monday", "Tuesday"};
+    public static final int[] FLIGHT_HOURS = new int[]{8, 10, 18, 20};
+    public static final String[] FLIGHT_LOCATIONS = new String[]{"Tehran", "Isfahan", "Yazd", "Mashhad", "Shiraz", "Kerman"};
+
+    private final String id;
+    private final String from;
+    private final String to;
+    private final String weekDay;
+    private final int hour;
+
+    public Flight(String id, String from, String to, String weekDay, int hour) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.weekDay = weekDay;
+        this.hour = hour;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s  %-10s%-10s%-10s%s",
+                BG_WHITE_BRIGHT + BLACK_BOLD + " " + id + "    " + BG_WHITE,
+                from,
+                to,
+                weekDay,
+                hour + (hour < 10 ? " " : "") + "   " + RESET
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return hour == flight.hour && from.equals(flight.from) && to.equals(flight.to) && weekDay.equals(flight.weekDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to, weekDay, hour);
+    }
 }
