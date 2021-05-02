4400330db53a1ec4c0a91519fb42cd4fe36ddb44
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-05-02 20:52:04 +0430

    Update AirlineData.java

--- src/dev/yekta/airline/AirlineData.java
+++ src/dev/yekta/airline/AirlineData.java
@@ -20,6 +20,104 @@
 
 package dev.yekta.airline;
 
-public class AirlineData {
+import java.util.ArrayList;
 
+public class AirlineData {
+    private static AirlineData airlineData;
+
+    public static AirlineData getInstance() {
+        if (airlineData == null)
+            airlineData = new AirlineData();
+
+        return airlineData;
+    }
+
+    private final ArrayList<User> users;
+    private final ArrayList<Flight> flights;
+    private int currentSessionUserId = -1;
+
+    public AirlineData() {
+        users = new ArrayList<>();
+        flights = new ArrayList<>();
+    }
+
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+    public void addUser(String username, String password) {
+        users.add(new User(username, password));
+    }
+
+    public void addFlight(Flight flight) {
+        flights.add(flight);
+    }
+
+    public boolean hasUser(String username) {
+        for (User u : users)
+            if (u.getUsername().equals(username))
+                return true;
+
+        return false;
+    }
+
+    public User currentUser() {
+        return users.get(currentSessionUserId);
+    }
+
+    public boolean login(String username, String password) {
+        for (User u : users)
+            if (u.getUsername().equals(username) && u.getPassword().equals(User.hashPassword(password))) {
+                currentSessionUserId = u.getId();
+                return true;
+            }
+
+        return false;
+    }
+
+    public void logout() {
+        currentSessionUserId = -1;
+    }
+
+    public ArrayList<Flight> findFlights(String from, String to, String weekDay, int hour) {
+        ArrayList<Flight> result = new ArrayList<>();
+
+        flights.stream().filter(f ->
+                f.getFrom().equalsIgnoreCase(from)
+                        && f.getTo().equalsIgnoreCase(to)
+                        && f.getWeekDay().equalsIgnoreCase(weekDay)
+                        && f.getHour() == hour
+        ).forEach(result::add);
+
+        return result;
+    }
+
+    public ArrayList<Flight> findFlights(String from, String to, String weekDay) {
+        ArrayList<Flight> result = new ArrayList<>();
+
+        flights.stream().filter(f ->
+                f.getFrom().equalsIgnoreCase(from)
+                        && f.getTo().equalsIgnoreCase(to)
+                        && f.getWeekDay().equalsIgnoreCase(weekDay)
+        ).forEach(result::add);
+
+        return result;
+    }
+
+    public ArrayList<Flight> findFlights(String from, String to) {
+        ArrayList<Flight> result = new ArrayList<>();
+
+        flights.stream().filter(f ->
+                f.getFrom().equalsIgnoreCase(from)
+                        && f.getTo().equalsIgnoreCase(to)
+        ).forEach(result::add);
+
+        return result;
+    }
+
+    public ArrayList<Flight> findFlights(String from) {
+        ArrayList<Flight> result = new ArrayList<>();
+        flights.stream().filter(f -> f.getFrom().equalsIgnoreCase(from)).forEach(result::add);
+        return result;
+    }
 }
