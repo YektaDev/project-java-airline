bc0ebe265a21b943ca5b223fb6f5cf87de057490
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-05-28 12:06:16 +0430

    Update User.java to work with files

--- src/dev/yekta/airline/User.java
+++ src/dev/yekta/airline/User.java
@@ -22,14 +22,34 @@ package dev.yekta.airline;
 
 import java.util.ArrayList;
 
-public class User {
+public class User implements Serializable<User>, Cloneable {
     private static int lastId = -1;
     private final int id;
     private final String username;
     private final String password;
-    private final ArrayList<Flight> flights;
+    private ArrayList<Flight> flights;
+
+    private int getLastId() {
+        if (lastId == -1)
+            return AirlineData.getInstance().countRealLines() - 1;
+        return lastId;
+    }
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.flights = new ArrayList<>();
+    }
+
+    public User() {
+        this.id = -1;
+        this.username = this.password = null;
+        this.flights = null;
+    }
 
     public User(String username, String password) {
+        lastId = getLastId();
         this.id = ++lastId;
         this.username = username;
         this.password = hashPassword(password);
@@ -52,11 +72,79 @@ public class User {
         return flights;
     }
 
-    public void addFlight(Flight flight) {
-        flights.add(flight);
-    }
-
     public static String hashPassword(String password) {
         return Hash.sha256(password);
+    }
+
+    public User updateFlights() {
+        FileManager<User> file = AirlineData.getInstance().getUsersFile();
+        file.open();
+
+        User current;
+        while (file.pointerPos() < file.length()) {
+            current = file.readRecord();
+            if (current.getId() == id) {
+                this.flights.clear();
+                this.flights.addAll(current.getFlights());
+                file.close();
+                return this;
+            }
+        }
+
+        file.close();
+        return null;
+    }
+
+    @Override
+    public String serialize() {
+        StringBuilder flightsString = new StringBuilder();
+
+        for (int i = 0; i < flights.size(); i++) {
+            flightsString.append(flights.get(i).serialize());
+
+            if (i < flights.size() - 1)
+                flightsString.append(SEP_W);
+        }
+
+        return id + SEP_W + username + SEP_W + password + ((flights.size() > 0) ? (SEP_W + flightsString) : "");
+    }
+
+    @Override
+    public User deserialize(String data) {
+        String[] userData = data.split(SEP_R);
+        ArrayList<Flight> userFlights = new ArrayList<>();
+
+        for (int i = 3; i <= userData.length - 2; i += 5) {
+            userFlights.add(
+                    new Flight(
+                            cleanup(userData[i]),
+                            cleanup(userData[i + 1]),
+                            cleanup(userData[i + 2]),
+                            cleanup(userData[i + 3]),
+                            Integer.parseInt(cleanup(userData[i + 4]))
+                    )
+            );
+        }
+
+        User u = new User(
+                Integer.parseInt(cleanup(userData[0])),
+                cleanup(userData[1]),
+                cleanup(userData[2])
+        ) {{
+            flights = new ArrayList<>();
+        }};
+
+        u.getFlights().addAll(userFlights);
+
+        return u;
+    }
+
+    public User clone() {
+        try {
+            return (User) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
